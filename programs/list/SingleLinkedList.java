abd2d0e53f156d995a17828eb050065f43d12d88
jskankurgarg <jskankurgarg@gmail.com>
2015-02-23 01:49:36 +0530

    Link List 1. add 2 numbers rept by linked list 2. max sum link list out of given 2 link list 3. detect loop in link list

--- programs/list/SingleLinkedList.java
+++ programs/list/SingleLinkedList.java
@@ -114,7 +114,7 @@ public class SingleLinkedList {
 	
 	public static Node mergeSort(Node list){
 		if(list != null && list.next != null){
-			Node mid = getMiddleElement(list);
+			Node mid = getMiddleNode(list);
 			Node midNext = mid.next;
 			mid.next = null;
 			list = mergeSort(list);
@@ -131,7 +131,7 @@ public class SingleLinkedList {
 	 * @param list
 	 * @return
 	 */
-	public static Node getMiddleElement(Node list){
+	public static Node getMiddleNode(Node list){
 		if(list == null || list.next == null){
 			return list;
 		}
@@ -211,30 +211,229 @@ public class SingleLinkedList {
 	
 	/**
 	 * API : reverse the given linked list.
+	 * TC : O(n) SC : O(n)
 	 * @param args
 	 */
 	public static Node reverse(Node list){
 		if(list == null || list.next == null) return list;
-		Node n = list;
+		Node current = list;
 		Node next = null;
-		Node p = null;
+		Node previous = null;
 
-		while(n != null){
-			next = n.next;
-			n.next = p;
-			p = n;
-			n = next;
+		while(current != null){
+			next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
 		}
-		return p;
+		return previous;
+	}
+	
+	/**
+	 * API has loop
+	 * TC : O(n) SC : O(n)
+	 * @param args
+	 */
+	public static boolean hasLoop(Node list){
+		if(list == null) return false;
+		Node current = list;
+		Node fast = list.next;
+		while(fast != null){
+			fast = fast.next;
+			if(fast != null){
+				fast = fast.next;
+				current = current.next;
+			}
+			if(fast == current){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Given two numbers represented by two lists, write a function that returns sum list. The sum list is list representation of addition of two input numbers.
+
+Example 1
+
+Input:
+  First List: 5->6->3  // represents number 3<-6<-5<-
+  Second List: 8->4->2 //  represents number 2<-4<-8<-
+Output
+  Resultant list: 3->1->6  // represents number 613
+Example 2
+
+Input:
+  First List: 7->5->9->4->6  // represents number 64957
+  Second List: 8->4 //  represents number 48
+Output
+  Resultant list: 5->0->0->5->6  // represents number 65005
+  NOTE : list link starts pointing the digits from right to left (this makes the algo elegant)
+  there can also be a case where link list is pointing the digits from left to right (will be reversed to use above approach)
+	 * @param args
+	 * TC : O(n), n is length of bigger number rept in form of link list
+	 */
+	public static Node addTwoNumbersRTL(Node x, Node y){
+		if(x == null) return y;
+		if(y == null) return x;
+		return addTwoNumbersRTL(x,y,0);
+	}
+	
+	private static Node addTwoNumbersRTL(Node x, Node y, int carry){
+		if(x == null && y == null && carry == 0) return null;
+
+		if(x == null)	x = new Node(0);
+		if(y == null)	y = new Node(0);
+		
+		int data = x.data + y.data + carry;
+		
+		carry = data / 10;
+		data = data % 10;
+		
+		Node z = new Node(data);
+		z.next = addTwoNumbersRTL(x.next, y.next, carry); 
+		
+		return z;
+	}
+	
+	/**
+	 * The Usual of way of passing data where link list points digits from left to right
+	 * @param x
+	 * @param y
+	 * TC : O(n), n is length of bigger number rept in form of link list
+	 * @return Sum 
+	 */
+	public static Node addTwoNumbersLTR(Node x, Node y){
+		x = reverse(x);
+		y = reverse(y);
+		Node z = addTwoNumbersRTL(x, y);
+		z = reverse(z);
+		return z;
+	}
+	
+	/**
+	 * @Question : 
+	 * Given two SORTED linked lists, construct a linked list that contains maximum sum path from start to end. The result list may contain nodes from both input lists. When constructing the result list, we may switch to the other input list only at the point of intersection (which mean the two node with the same value in the lists). You are allowed to use O(1) extra space.
+
+Input:
+List1 =  1->3->30->90->120->240->511
+List2 =  0->3->12->32->90->125->240->249
+
+Output: Following is maximum sum linked list out of two input lists
+list =  1->3->12->32->90->125->240->511
+we switch at 3 and 240 to get above maximum sum linked list
+	 * @param args
+	 * TC : O(n) length of longer link list
+	 */
+	public static Node maxSumLinkList(Node a, Node b){
+		if(a == null)	return b;
+		if(b == null)	return a;
+		
+		Node c = new Node(0);
+		
+		Node currentA = a;
+		Node previousA = a;
+		Node currentB = b;
+		Node previousB = b;
+		Node currentC = c;
+
+		int sumA = 0;
+		int sumB = 0;
+		
+		while(currentA != null && currentB != null){
+			if(currentA.compareTo(currentB) == 0){ // found a common point b/w 2 lists
+				if(sumA < sumB){
+					currentC.next = previousB;
+					currentC = currentB;
+				}else{
+					currentC.next = previousA;
+					currentC = currentA;
+				}
+				previousA = currentA.next;
+				previousB = currentB.next;
+				sumA = 0;
+				sumB = 0;
+				currentA = currentA.next;
+				currentB = currentB.next;
+			}
+			else if(currentA.compareTo(currentB) < 0){
+				sumA += currentA.data;
+				currentA = currentA.next;
+			}
+			else{
+				sumB += currentB.data;
+				currentB = currentB.next;
+			}
+		}
+		if(currentA != null){
+			currentC.next = previousA;
+		}
+		if(currentB != null){
+			currentC.next = previousB;
+		}
+		
+		return c.next;
 	}
 	
 	public static void main(String[] args) {
-		testReverse();
+		testMaxSumLinkList();
+//		testAddTwoLinkList();
+//		testLoop();
+//		testReverse();
 //		testMiddleElement();
 //		testRemoveDuplicates();
 //		testmergeSortedList();
 //		testMergeSort();
 //		testSwap();
+	}
+	
+	private static void testMaxSumLinkList(){
+		Node m = new Node(1);
+		m.next = new Node(3);
+		m.next.next = new Node(30);
+		m.next.next.next = new Node(90);
+		m.next.next.next.next = new Node(120);
+		m.next.next.next.next.next = new Node(240);
+		m.next.next.next.next.next.next = new Node(511);
+		
+		Node n = new Node(0);
+		n.next = new Node(3);
+		n.next.next = new Node(12);
+		n.next.next.next = new Node(32);
+		n.next.next.next.next = new Node(90);
+		n.next.next.next.next.next = new Node(125);
+		n.next.next.next.next.next.next = new Node(240);
+		
+		Node.printLinkList(m);
+		Node.printLinkList(n);
+		
+		Node.printLinkList(maxSumLinkList(m, n));
+		 
+	}
+	
+	private static void testAddTwoLinkList(){
+		Node x = new Node(5);
+		x.next = new Node(6);
+		x.next.next = new Node(9);
+		
+		Node y = new Node(7);
+		y.next = new Node(8);
+		
+		Node.printLinkList(x);
+		Node.printLinkList(y);
+		
+		Node.printLinkList(addTwoNumbersRTL(x, y));
+		Node.printLinkList(addTwoNumbersLTR(x, y));
+	}
+	
+	private static void testLoop(){
+		Node list = new Node(10);
+		list.next = list;//new Node(20);
+//		list.next.next = list;//new Node(30);
+//		list.next.next.next = new Node(40);
+//		list.next.next.next.next = list.next;
+//		Node.printLinkList(list);
+		System.out.println("hasLoop = " + hasLoop(list));
 	}
 	
 	private static void testReverse(){
@@ -264,7 +463,7 @@ public class SingleLinkedList {
 		list.next.next.next = new Node(120);
 		list.next.next.next.next = new Node(20);
 		Node.printLinkList(list);
-		System.out.println(getMiddleElement(list));
+		System.out.println(getMiddleNode(list));
 	}
 	
 	private static void testRemoveDuplicates(){
