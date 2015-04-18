0aa1d692f1c77347ffc19ec2e280da0d81b91255
ankugarg <ankugarg@paypal.com>
2015-04-18 12:13:28 +0530

    Fixed median bug

--- programs/array/MedianOf2Arrays.java
+++ programs/array/MedianOf2Arrays.java
@@ -19,23 +19,53 @@ There are different conventions to take median of an array with even number of e
  */
 public class MedianOf2Arrays {
 	
+	/**
+	 * Divide and Conquer approach
+	 * @param arr1
+	 * @param l1
+	 * @param h1
+	 * @param arr2
+	 * @param l2
+	 * @param h2
+	 * @return
+	 * TC : O(logN)
+	 */
 	public static int medianAlgo_O_Logn(int[] arr1, int l1, int h1, int[] arr2, int l2, int h2){
+
+		//negative base case
+		if(l1 > h1){
+			return -1;
+		}
+		
 		int mid1 = Math.floorDiv(l1+h1,2);
 		int mid2 = Math.floorDiv(l2+h2,2);
-		int med1 =  arr1[mid1];
-		int med2 =  arr2[mid2];
-		@SuppressWarnings("unused")
-		int y=90+mid1;
+		int med1 =  getMedian(arr1, l1, h1);
+		int med2 =  getMedian(arr2, l2, h2);
+		int length = h1-l1+1;
+
 		if(l1 == h1 && l2 == h2){
-			return Math.min(med1, med2);
+			return (med1+med2)/2;
 		}
 
 		if(med1 == med2){
 			return med1;
-		}else if(med1 < med2){
-			return medianAlgo_O_Logn(arr1, mid1+1, h1, arr2, l2, mid2);
+		}else if(h1-l1 == 1 && h2-l2 == 1){
+			return (Math.max(arr1[l1], arr2[l2])+Math.min(arr1[h1], arr2[h2])) / 2;
+		}
+		else if(med1 < med2){
+			if(length%2 == 0){
+				return medianAlgo_O_Logn(arr1, mid1+1, h1, arr2, l2, mid2);
+			}
+			else{
+				return medianAlgo_O_Logn(arr1, mid1, h1, arr2, l2, mid2);
+			}
 		}else{
-			return medianAlgo_O_Logn(arr1, l1, mid1, arr2, mid2+1, h2);
+			if(length%2 == 0){
+				return medianAlgo_O_Logn(arr1, l1, mid1, arr2, mid2+1, h2);
+			}
+			else{
+				return medianAlgo_O_Logn(arr1, l1, mid1, arr2, mid2, h2);
+			}
 		}
 	}
 	
@@ -61,15 +91,40 @@ public class MedianOf2Arrays {
 		return (previous+current)/2.0f;
 	}
 	
+	public static int getMedian(int [] arr, int start, int end){
+		if(start > end){
+			return -1;
+		}
+		
+		if(start == end){
+			return arr[start];
+		}
+		int mid = (start +  (end-start)/2);
+		if((end-start)%2 ==0 ){//ODD array size
+			return arr[mid];
+		}else{
+			return (arr[mid]+arr[mid+1])/2;
+		} 
+	}
+	
 	public static void main(String...args){
 		tc1();
+		//tc2();
+	}
+	
+	
+	public static void tc2(){
+		int[] arr1 = new int[]{10,20,30};
+		int[] arr2 = new int[]{10,25,30,60};
+		System.out.println("median1="+getMedian(arr1, 0, arr1.length-1));
+		System.out.println("median2="+getMedian(arr2, 0, arr2.length-1));
 	}
 	
 	public static void tc1(){
-		int[] arr1 = new int[]{10,20,30};
-		int[] arr2 = new int[]{5,15,25};
-//		System.out.println("median of 2 arrays : " + medianAlgo_O_Logn(arr1, 0, arr1.length-1, arr2, 0, arr2.length-1));
+		int[] arr1 = new int[]{10,22,30,50};
+		int[] arr2 = new int[]{5,15,25,70};
+		System.out.println("median of 2 arrays : " + medianAlgo_O_Logn(arr1, 0, arr1.length-1, arr2, 0, arr2.length-1));
 		
-		System.out.println("median of 2 arrays : " + medianAlgo_O_n(arr1, arr2));
+		//System.out.println("median of 2 arrays : " + medianAlgo_O_n(arr1, arr2));
 	}
 }
